39c5167288fba25be669e6d89393f0d447af6e05
Irina Merkulova <merkulovairina1985@gmail.com>
2023-11-27 02:00:10 +0300

    005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ирина")
+{
+    Console.WriteLine("Здравствуй, дорогая. Рада тебе!");
+}
+else
+{
+    Console.Write("Хай, ");
+    Console.WriteLine(username);
+}
